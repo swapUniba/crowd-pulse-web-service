7ab35047521709beba5022b75b61550bdd712069
CosimoLovascio <cosimolovascio@hotmail.it>
2017-12-22 10:17:50 +0100

    Added new endpoints (for Twitter and authentication)

---
+++ endpoint/auth.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var router = require('express').Router();
+var CrowdPulse = require('./../crowd-pulse-data');
+var config = require('./../lib/config');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+
+const DB_PROFILES = "profiles";
+
+const TOKEN_EXPIRE = 432000; // five days
+
+module.exports = function() {
+
+  /**
+   * Performs the login.
+   * Params:
+   *    email - the user email
+   *    password - the user password
+   */
+  router.route('/login')
+    .post(function (req, res) {
+      try {
+        var dbConn = new CrowdPulse();
+        return dbConn.connect(config.database.url, DB_PROFILES).then(function (conn) {
+          return conn.Profile.findOne({email: req.body.email}, function (err, user) {
+            if (!user) {
+              res.status(401);
+              res.json({
+                auth: false,
+                message: "Invalid email and/or password."
+              });
+            } else {
+              bcrypt.compare(req.body.password, user.password, function (err, isMatch) {
+                if (!isMatch) {
+                  res.status(401);
+                  res.json({
+                    auth: false,
+                    message: "Invalid email and/or password."
+                  });
+                } else {
+                  var token = jwt.sign({email: user.email}, config.session.secret, {expiresIn: TOKEN_EXPIRE});
+                  res.send({
+                    auth: true,
+                    token: token
+                  });
+                }
+              });
+            }
+          });
+        }).finally(function () {
+          dbConn.disconnect();
+        });
+      } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+    });
+
+  /**
+   * Register a new user.
+   * Params:
+   *    username - the username
+   *    email - the user email
+   *    password - the user password
+   */
+  router.route('/signup')
+    .post(function (req, res) {
+      try {
+        var dbConn = new CrowdPulse();
+        return dbConn.connect(config.database.url, DB_PROFILES).then(function (conn) {
+          return conn.Profile.findOne({email: req.body.email}, function (err, userEmail) {
+            if (userEmail) {
+              res.status(409);
+              res.json({
+                auth: false,
+                message: 'Email is already taken'
+              });
+            } else {
+              conn.Profile.findOne({displayName: req.body.displayName}, function (err, userDisplayName) {
+                if (userDisplayName) {
+                  res.status(409);
+                  res.json({
+                    auth: false,
+                    message: 'Username is already taken'
+                  });
+                } else {
+
+                  // encrypt password
+                  var salt = bcrypt.genSaltSync(10);
+                  var encryptedPassword = bcrypt.hashSync(req.body.password, salt);
+                  var user = {
+                    email: req.body.email,
+                    displayName: req.body.displayName,
+                    password: encryptedPassword
+                  };
+                  conn.Profile.newFromObject(user).save().then(function () {
+                    var token = jwt.sign({email: user.email}, config.session.secret, {expiresIn: TOKEN_EXPIRE});
+                    res.send({
+                      auth: true,
+                      token: token
+                    });
+                  });
+                }
+              });
+            }
+          });
+        }).finally(function () {
+          dbConn.disconnect();
+        });
+      } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+    });
+
+  return router;
+};
---
+++ endpoint/twitter.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var router = require('express').Router();
+var request = require('request');
+var qs = require('querystring');
+var CrowdPulse = require('./../crowd-pulse-data');
+var config = require('./../lib/config');
+
+const CONSUMER_KEY = 'UwKgjmP3nkgswMi18fFRMO5Kc';
+const CONSUMER_SECRET = 'gJ0NEoKovmNum8AXb9zstwYCcdU8WqUK0GnTrWx9kXWeFYSCAX';
+
+const API_REQUEST_TOKEN = 'https://api.twitter.com/oauth/request_token';
+const API_ACCESS_TOKEN = 'https://api.twitter.com/oauth/access_token';
+const API_AUTHENTICATION = 'https://api.twitter.com/oauth/authenticate';
+const API_TIMELINE = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+
+module.exports = function() {
+
+  /**
+   * Obtain a request token.
+   * Params:
+   *    callbackUrl - the url send to Twitter as callback
+   */
+  router.route('/twitter/request_token')
+    .post(function (req, res) {
+      try {
+        var oauth = {
+          consumer_key: CONSUMER_KEY,
+          consumer_secret: CONSUMER_SECRET,
+          callback: req.body.callback
+        };
+        request.post({url: API_REQUEST_TOKEN, oauth: oauth}, function (error, response, body) {
+          var authData = qs.parse(body);
+          var url = API_AUTHENTICATION + '?' + qs.stringify({oauth_token: authData.oauth_token});
+          if (authData.oauth_callback_confirmed === 'true') {
+            res.status(200);
+            res.json({
+              auth: true,
+              redirectUrl: url
+            });
+          } else {
+            res.sendStatus(500);
+          }
+        });
+      } catch(err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+    });
+
+  /**
+   * Render the request token into a usable access token.
+   * Params:
+   *    oauthToken - obtained after the user authentication
+   *    oauthVerifier - obtained after the user authentication
+   */
+  router.route('/twitter/access_token')
+    .post(function (req, res) {
+      var oauth = {
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET,
+        token: req.body.oauthToken,
+        verifier: req.body.oauthVerifier
+      };
+      request.post({url: API_ACCESS_TOKEN, oauth: oauth}, function (error, response, body) {
+        var oauthData = qs.parse(body);
+
+        // TODO save oauthData and reuse for future api call
+
+        res.status(200);
+
+        // TODO temporary code
+        res.json({
+          oauthToken: oauthData.oauth_token,
+          oauthTokenSecret: oauthData.oauth_token_secret
+        });
+      });
+    });
+
+  /**
+   * Gets the user timeline.
+   * Params:
+   *    oauthToken - obtained after the access token request
+   *    oauthTokenSecret - obtained after the access token request
+   */
+  router.route('/twitter/user_timeline')
+    .post(function (req, res) {
+      var oauth = {
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET,
+        token: req.body.oauthToken,
+        token_secret: req.body.oauthTokenSecret
+      };
+
+      request.get({url: API_TIMELINE, oauth: oauth, json:true}, function (error, response, tweets) {
+        console.log(tweets);
+        res.status(200);
+      });
+
+      // TODO delete this code
+      /*
+      var url = 'https://api.twitter.com/1.1/users/show.json';
+      var params = {
+        screen_name: oauthData.screen_name,
+        user_id: oauthData.user_id
+      };
+      request.get({url:url, oauth:oauth, qs:params, json:true}, function (err, response, user) {
+        console.log(user)
+      }) */
+    });
+
+
+  return router;
+};
