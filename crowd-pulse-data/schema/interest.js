a1a9705f9cb8867622bb02c768244867b6d734c3
CosimoLovascio <cosimolovascio@hotmail.it>
2018-02-21 10:11:33 +0100

    Added interests batch operation

--- crowd-pulse-data/schema/interest.js
+++ crowd-pulse-data/schema/interest.js
@@ -5,10 +5,17 @@ var builder = require('./schemaBuilder');
 var schemas = require('./schemaName');
 
 var InterestSchema = builder(schemas.token, {
-  id: String,
+  id: mongoose.Schema.ObjectId,
+  oId: String,
   source: String,
   timestamp: Number,
   confidence: Number
 });
 
 module.exports = InterestSchema;
+
+// Model methods
+
+InterestSchema.statics.newFromObject = function(object) {
+  return new this(object);
+};
