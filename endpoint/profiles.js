eae6b973c72cf05a9fb314e1c5ab76871e4a53f7
Roberto B. Stanziale <stanziale.r@outlook.com>
2018-10-10 19:09:40 +0200

    Fix API

--- endpoint/profiles.js
+++ endpoint/profiles.js
@@ -89,7 +89,7 @@ module.exports = function() {
             let mode;
 
             // Limit (req.query.l):
-            let l = Number.MAX_SAFE_INTEGER;
+            let l = Number.MAX_SAFE_INTEGER/4;
 
             if(req.query.l > 0) {
                 l = parseInt(req.query.l);
@@ -124,24 +124,24 @@ module.exports = function() {
                     "@context": "",
                     user: req.params.username,
 
-                    demographics: "Information not shared by the user", // From Profile.demographics collection
-                    affects: "Information not shared by the user", // From Message (Sentiment + Emotion) collection
+                    demographics: {}, // From Profile.demographics collection
+                    affects: {}, // From Message (Sentiment + Emotion) collection
                     behaviors: {
-                        fromText: "Information not shared by the user",
-                        fromActivity: "Information not shared by the user"
+                        fromText: {},
+                        fromActivity: {}
                     }, // From Message for "fromText" collection and from PersonalData for "fromActivity"
                     cognitiveAspects: {
-                        personalities: "Information not shared by the user",
-                        empathies: "Information not shared by the user"
+                        personalities: {},
+                        empathies: {}
                     }, // From Profile.personalities and Profile.empathies collection
-                    interests: "Information not shared by the user", // From Interest collection
+                    interests: {}, // From Interest collection
                     physicalStates: {
-                        heart: "Information not shared by the user",
-                        sleep: "Information not shared by the user",
-                        food: "Information not shared by the user",
-                        body: "Information not shared by the user"
+                        heart: {},
+                        sleep: {},
+                        food: {},
+                        body: {}
                     }, // From PersonalData, heart-rate, sleep, food and body
-                    socialRelations: "Information not shared by the user" // From Connection collection
+                    socialRelations: {} // From Connection collection
                 };
 
                 if (req.query.mode) {
@@ -177,7 +177,6 @@ module.exports = function() {
                                         if (user.demographics) {
                                             myData.demographics = user.demographics;
                                         }
-                                        else myData.demographics = "Missing information";
                                     }
                                 }
                                 dbConn.disconnect();
@@ -205,7 +204,7 @@ module.exports = function() {
                                                                         { $lte: [ "$$p.timestamp", maxDate.getTime()/1000 ] }
                                                                     ]}
                                                             }
-                                                        }, l]
+                                                        }, l/2]
                                                     }
                                                 }
                                             }
@@ -213,7 +212,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.cognitiveAspects.personalities = profile[0]["personalities"];
                                             }
-                                            else myData.cognitiveAspects.personalities = "Missing information";
                                         });
                                     })
                                     .finally(function () {
@@ -243,7 +241,7 @@ module.exports = function() {
                                                                         { $lte: [ "$$e.timestamp", maxDate.getTime()/1000 ] }
                                                                     ]}
                                                             }
-                                                        }, l]
+                                                        }, l/2]
                                                     }
                                                 }
                                             }
@@ -251,7 +249,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.cognitiveAspects.empathies = profile[0]["empathies"];
                                             }
-                                            else myData.cognitiveAspects.empathies = "Missing information";
                                         });
                                     })
                                     .finally(function () {
@@ -277,7 +274,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.affects = profile;
                                             }
-                                            else myData.affects = "Missing information";
                                         }).limit(parseInt(l));
                                     })
                                     .finally(function () {
@@ -304,7 +300,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.behaviors.fromText = profile;
                                             }
-                                            else myData.behaviors.fromText = "Missing information";
                                         }).limit(parseInt(l));
                                     })
                                     .finally(function () {
@@ -341,7 +336,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.behaviors.fromActivity = profile;
                                             }
-                                            else myData.behaviors.fromActivity = "Missing information";
                                         }).limit(parseInt(l));
                                     })
                                     .finally(function () {
@@ -367,7 +361,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.interests = profile;
                                             }
-                                            else myData.interests = "Missing information";
                                         }).limit(parseInt(l));
                                     })
                                     .finally(function () {
@@ -422,7 +415,6 @@ module.exports = function() {
                                                     if (profile) {
                                                         myData.physicalStates.sleep = profile;
                                                     }
-                                                    else myData.physicalStates.sleep = "Missing information";
                                                 }).limit(parseInt(l));
                                             })
                                     })
@@ -448,7 +440,6 @@ module.exports = function() {
                                                     if (profile) {
                                                         myData.physicalStates.food = profile;
                                                     }
-                                                    else myData.physicalStates.food = "Missing information";
                                                 }).limit(parseInt(l));
                                             })
                                     })
@@ -470,7 +461,6 @@ module.exports = function() {
                                                     if (profile) {
                                                         myData.physicalStates.body = profile;
                                                     }
-                                                    else myData.physicalStates.body = "Missing information";
                                                 }).limit(parseInt(l));
                                             })
                                     })
@@ -494,7 +484,6 @@ module.exports = function() {
                                             if (profile) {
                                                 myData.socialRelations = profile;
                                             }
-                                            else myData.socialRelations = "Missing information";
                                         }).limit(parseInt(l));
                                     })
                                     .finally(function () {
